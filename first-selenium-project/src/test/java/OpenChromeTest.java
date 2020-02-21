5fa0ad608e26074f5fba902ce081634011765980
Vladimir <voldyman@gmail.com>
2020-02-21 16:49:53 +0200

    first commit

---
+++ first-selenium-project/src/test/java/OpenChromeTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class OpenChromeTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+    @Test
+    public void openChromeTest(){
+        driver.navigate().to("https://www.google.com/");
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
