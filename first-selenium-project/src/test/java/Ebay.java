145591023631bcc893dd59df377b873a9c7a2692
Vladimir <voldyman@gmail.com>
2020-02-22 15:23:32 +0200

    added ebay module

---
+++ first-selenium-project/src/test/java/Ebay.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Ebay {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+    @Test
+    public void openChromeTest() {
+        driver.navigate().to("https://www.ebay.com/");
+        driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
+        driver.findElement(By.name("userid")).sendKeys("voldyman@gmail.com");
+        driver.findElement(By.name("pass")).sendKeys("Eb06102007");
+        driver.findElement(By.id("sgnBt")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
--- first-selenium-project/src/test/java/OpenChromeTest.java
+++ first-selenium-project/src/test/java/OpenChromeTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -11,7 +12,7 @@ public class OpenChromeTest {
     WebDriver driver;
     @BeforeMethod
     public void setUp(){
-        driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
     @Test
